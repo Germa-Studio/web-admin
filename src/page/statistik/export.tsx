5fef589b196c2680c6833edb98d9c406191deaea
Irwnda <irwandaa7@gmail.com>
2024-09-14 22:17:03 +0700

    feat: immediately export file

--- src/page/statistik/export.tsx
+++ src/page/statistik/export.tsx
@@ -1,4 +1,4 @@
-import { Anchor, Breadcrumbs, Button } from '@mantine/core';
+import { Anchor, Breadcrumbs } from '@mantine/core';
 import React, { useEffect } from 'react';
 import { GetStatistikTanamanAll } from '../../infrastucture/statistic';
 import { PaginatedRespApiData } from '../../types/paginatedRespApi';
@@ -18,250 +18,229 @@ const breadcrumbItems = [
 
 export default function ExportTable() {
   const [resp, setResp] = React.useState<PaginatedRespApiData<TDataTanaman> | undefined>();
-
-  const tbl = React.useRef(null);
+  const [completeMessage, setCompleteMessage] = React.useState<string | undefined>();
 
   useEffect(() => {
     GetStatistikTanamanAll(undefined, {
       isExport: true
     }).then((res) => {
-      console.log(res.data);
       setResp(res?.data);
     });
+  }, []);
+
+  useEffect(() => {
+    if (!resp) return;
+
+    const blankCell = (numberOfCol: number) => {
+      return Array.from({ length: numberOfCol }).map(() => '-');
+    };
+
+    const headers = [
+      'KOMODITAS',
+      'LUAS TANAM (HA)',
+      'BULAN TANAM',
+      'PRAKIRAAN BULAN PANEN',
+      'PRAKIRAAN LUAS PANEN (HA)',
+      'PRAKIRAAN HASIL PANEN (TON)',
+      'REALISASI LUAS PANEN (HA)',
+      'REALISASI PRODUKSI PANEN (TON)'
+    ];
+
+    const result = resp.data.map((item) => {
+      const kategori = item.kategori.toLocaleLowerCase();
+      const row = [
+        item.fk_kelompokId,
+        item.kelompok?.kecamatan,
+        item.kelompok?.desa,
+        item.luasLahan,
+        item.kelompok?.gapoktan,
+        item.kelompok?.namaKelompok
+      ];
+
+      if (kategori.includes('pangan')) {
+        const data = [
+          item.komoditas,
+          item.luasLahan,
+          item.periodeTanam,
+          item.prakiraanBulanPanen,
+          item.prakiraanLuasPanen,
+          item.prakiraanHasilPanen,
+          item.realisasiLuasPanen ?? '-',
+          item.realisasiHasilPanen ?? '-'
+        ];
+        row.push(...data);
+      } else {
+        row.push(...blankCell(8));
+      }
+
+      if (
+        kategori.includes('kebun') &&
+        komoditasSemusim.includes(item.komoditas.replace('Buah ', ''))
+      ) {
+        const data = [
+          item.komoditas,
+          item.luasLahan,
+          item.periodeTanam,
+          item.prakiraanBulanPanen,
+          item.prakiraanLuasPanen,
+          item.prakiraanHasilPanen,
+          item.realisasiLuasPanen ?? '-',
+          item.realisasiHasilPanen ?? '-'
+        ];
+        row.push(...data);
+      } else {
+        row.push(...blankCell(8));
+      }
+
+      if (
+        kategori.includes('kebun') &&
+        (komoditasTahunan.includes(item.komoditas.replace('Sayur ', '')) ||
+          ['Perkebunan Tembakau', 'Perkebunan Tebu'].includes(item.komoditas))
+      ) {
+        const data = [
+          item.komoditas,
+          item.luasLahan,
+          item.prakiraanBulanPanen,
+          item.prakiraanLuasPanen,
+          item.prakiraanHasilPanen,
+          item.realisasiLuasPanen ?? '-'
+        ];
+        row.push(...data);
+      } else {
+        row.push(...blankCell(6));
+      }
+
+      if (
+        (kategori.includes('sayur') || kategori.includes('buah')) &&
+        komoditasSemusim.includes(item.komoditas.replace('Buah ', ''))
+      ) {
+        const data = [
+          item.komoditas,
+          item.luasLahan,
+          item.periodeTanam,
+          item.prakiraanBulanPanen,
+          item.prakiraanLuasPanen,
+          item.prakiraanHasilPanen,
+          item.realisasiBulanPanen ?? '-',
+          item.realisasiLuasPanen ?? '-',
+          item.realisasiHasilPanen ?? '-'
+        ];
+        row.push(...data);
+      } else {
+        row.push(...blankCell(9));
+      }
+
+      if (
+        (kategori.includes('sayur') || kategori.includes('buah')) &&
+        komoditasTahunan.includes(item.komoditas.replace('Sayur ', ''))
+      ) {
+        const data = [
+          item.komoditas,
+          item.luasLahan,
+          item.prakiraanBulanPanen,
+          item.prakiraanHasilPanen,
+          item.realisasiLuasPanen ?? '-',
+          item.realisasiHasilPanen ?? '-'
+        ];
+        row.push(...data);
+      } else {
+        row.push(...blankCell(6));
+      }
+
+      return row;
+    });
+
+    const data = [
+      [
+        'NO POKTAN',
+        'KECAMATAN',
+        'DESA',
+        'LAHAN BAKU',
+        'GAPOKTAN',
+        'NAMA POKTAN',
+        'TANAMAN PANGAN',
+        ...blankCell(7),
+        'TANAMAN PERKEBUNAN SEMUSIM',
+        ...blankCell(7),
+        'TANAMAN PERKEBUNAN TAHUNAN',
+        ...blankCell(5),
+        'TANAMAN HORTIKULTURA SEMUSIM',
+        ...blankCell(8),
+        'TANAMAN HORTIKULTURA TAHUNAN'
+      ],
+      [
+        ...blankCell(6),
+        ...headers,
+        ...headers,
+        ...headers.filter(
+          (header) => header !== 'BULAN TANAM' && header !== 'REALISASI PRODUKSI PANEN (TON)'
+        ),
+        ...headers.slice(0, 6),
+        'REALISASI BULAN PANEN',
+        ...headers.slice(6),
+        ...headers.filter(
+          (header) => header !== 'BULAN TANAM' && header !== 'PRAKIRAAN LUAS PANEN (HA)'
+        )
+      ],
+      ...result
+    ];
+
+    const worksheet = utils.aoa_to_sheet(data);
+
+    worksheet['!merges'] = [
+      { s: { r: 0, c: 0 }, e: { r: 1, c: 0 } }, // NO POKTAN (A1:A2)
+      { s: { r: 0, c: 1 }, e: { r: 1, c: 1 } }, // KECAMATAN (B1:B2)
+      { s: { r: 0, c: 2 }, e: { r: 1, c: 2 } }, // DESA (C1:C2)
+      { s: { r: 0, c: 3 }, e: { r: 1, c: 3 } }, // LAHAN BAKU (D1:D2)
+      { s: { r: 0, c: 4 }, e: { r: 1, c: 4 } }, // GAPOKTAN (E1:E2)
+      { s: { r: 0, c: 5 }, e: { r: 1, c: 5 } }, // NAMA POKTAN (F1:F2)
+
+      // Merging for TANAMAN PANGAN
+      { s: { r: 0, c: 6 }, e: { r: 0, c: 13 } }, // TANAMAN PANGAN (G1:N1)
+
+      // Merging for TANAMAN PERKEBUNAN SEMUSIM
+      { s: { r: 0, c: 14 }, e: { r: 0, c: 21 } }, // TANAMAN PERKEBUNAN SEMUSIM (O1:V1)
+
+      // Merging for TANAMAN PERKEBUNAN TAHUNAN
+      { s: { r: 0, c: 22 }, e: { r: 0, c: 27 } }, // TANAMAN PERKEBUNAN TAHUNAN (W1:AB1)
+
+      // Merging for TANAMAN HORTIKULTURA SEMUSIM
+      { s: { r: 0, c: 28 }, e: { r: 0, c: 36 } }, // TANAMAN HORTIKULTURA SEMUSIM (AC1:AK1)
+
+      // Merging for TANAMAN HORTIKULTURA TAHUNAN
+      { s: { r: 0, c: 37 }, e: { r: 0, c: 42 } } // TANAMAN HORTIKULTURA TAHUNAN (AL1:AQ1)
+    ];
+
+    // Create a new workbook
+    const workbook = utils.book_new();
+
+    utils.book_append_sheet(workbook, worksheet);
+    writeFileXLSX(
+      workbook,
+      `Data Tanaman - ${new Date().toLocaleDateString('id-ID', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+      })}.xlsx`
+    );
+  }, [resp]);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setCompleteMessage('Anda dapat mengunduh file data jika proses telah selesai.');
+    }, 3000);
+
+    return () => clearTimeout(timeoutId);
   }, []);
 
   return (
     <div>
       <Breadcrumbs>{breadcrumbItems}</Breadcrumbs>
-      <div className="flex justify-end">
-        <Button
-          variant="filled"
-          onClick={() => {
-            // console.log(utils, writeFileXLSX);
-            const wb = utils.table_to_book(tbl.current);
-            writeFileXLSX(
-              wb,
-              `Data Tanaman - ${new Date().toLocaleDateString('id-ID', {
-                year: 'numeric',
-                month: 'long',
-                day: 'numeric'
-              })}.xlsx`
-            );
-          }}>
-          Export
-        </Button>
-      </div>
-      <div className="overflow-x-scroll">
-        <table ref={tbl} className="table-auto w-fit mt-2 bg-white">
-          <thead className="[&_*]:whitespace-nowrap">
-            <tr className="[&>th]:text-center">
-              <th className="border p-2" rowSpan={2}>
-                NO POKTAN
-              </th>
-              <th className="border p-2" rowSpan={2}>
-                KECAMATAN
-              </th>
-              <th className="border p-2" rowSpan={2}>
-                DESA
-              </th>
-              <th className="border p-2" rowSpan={2}>
-                LAHAN BAKU
-              </th>
-              <th className="border p-2" rowSpan={2}>
-                GAPOKTAN
-              </th>
-              <th className="border p-2" rowSpan={2}>
-                NAMA POKTAN
-              </th>
-              <th
-                className="border p-2 text-white"
-                colSpan={8}
-                style={{ backgroundColor: 'rgb(123, 97, 23)' }}>
-                TANAMAN PANGAN
-              </th>
-              <th
-                className="border p-2 text-white"
-                colSpan={8}
-                style={{ backgroundColor: 'rgb(44, 77, 117)' }}>
-                TANAMAN PERKEBUNAN SEMUSIM
-              </th>
-              <th
-                className="border p-2 text-white"
-                colSpan={6}
-                style={{ backgroundColor: 'rgb(122, 64, 26)' }}>
-                TANAMAN PERKEBUNAN TAHUNAN
-              </th>
-              <th
-                className="border p-2 text-white"
-                colSpan={9}
-                style={{ backgroundColor: 'rgb(82, 82, 82)' }}>
-                TANAMAN HORTIKULTURA SEMUSIM
-              </th>
-              <th
-                className="border p-2 text-white"
-                colSpan={6}
-                style={{ backgroundColor: 'rgb(36, 43, 52)' }}>
-                TANAMAN HORTIKULTURA TAHUNAN
-              </th>
-            </tr>
-            <tr className="[&>th]:text-left">
-              {/* Tanaman Pangan */}
-              <th className="border p-2">KOMODITAS</th>
-              <th className="border p-2">LUAS TANAM (HA)</th>
-              <th className="border p-2">BULAN TANAM</th>
-              <th className="border p-2">PRAKIRAAN BULAN PANEN</th>
-              <th className="border p-2">PRAKIRAAN LUAS PANEN (HA)</th>
-              <th className="border p-2">PRAKIRAAN HASIL PANEN (TON)</th>
-              <th className="border p-2">REALISASI LUAS PANEN (HA)</th>
-              <th className="border p-2">REALISASI PRODUKSI PANEN (TON)</th>
-              {/* Tanaman Perkebunan Semusim */}
-              {/* KOMODITAS	LUAS TANAM (HA)	BULAN TANAM	PRAKIRAAN BULAN PANEN	PRAKIRAAN LUAS PANEN (HA)	PRAKIRAAN HASIL PANEN (TON)	REALISASI LUAS PANEN (HA)	REALISASI HASIL PANEN (TON) */}
-              <th className="border p-2">KOMODITAS</th>
-              <th className="border p-2">LUAS TANAM (HA)</th>
-              <th className="border p-2">BULAN TANAM</th>
-              <th className="border p-2">PRAKIRAAN BULAN PANEN</th>
-              <th className="border p-2">PRAKIRAAN LUAS PANEN (HA)</th>
-              <th className="border p-2">PRAKIRAAN HASIL PANEN (TON)</th>
-              <th className="border p-2">REALISASI LUAS PANEN (HA)</th>
-              <th className="border p-2">REALISASI HASIL PANEN (TON)</th>
-              {/* Tanaman Perkebunan Tahunan */}
-              {/* KOMODITAS	"LUAS TANAMAN (HA)"	PRAKIRAAN BULAN PANEN	PRAKIRAAN PRODUKSI PANEN (KW)	"LUAS PANEN (HA)"	REALISASI PRODUKSI PANEN (KW) */}
-              <th className="border p-2">KOMODITAS</th>
-              <th className="border p-2">LUAS TANAM (HA)</th>
-              <th className="border p-2">PRAKIRAAN BULAN PANEN</th>
-              <th className="border p-2">PRAKIRAAN LUAS PANEN (HA)</th>
-              <th className="border p-2">PRAKIRAAN HASIL PANEN (TON)</th>
-              <th className="border p-2">REALISASI LUAS PANEN (HA)</th>
-              {/* Tanaman Hortikultura Semusim */}
-              {/* KOMODITAS	LUAS TANAM (HA)	BULAN TANAM	PRAKIRAAN BULAN PANEN	PRAKIRAAN LUAS PANEN (HA)	PRAKIRAAN HASIL PANEN (KW)	REALISASI BULAN PANEN	REALISASI LUAS PANEN (HA)	REALISASI PRODUKSI PANEN (KW) */}
-              <th className="border p-2">KOMODITAS</th>
-              <th className="border p-2">LUAS TANAM (HA)</th>
-              <th className="border p-2">BULAN TANAM</th>
-              <th className="border p-2">PRAKIRAAN BULAN PANEN</th>
-              <th className="border p-2">PRAKIRAAN LUAS PANEN (HA)</th>
-              <th className="border p-2">PRAKIRAAN HASIL PANEN (KW)</th>
-              <th className="border p-2">REALISASI BULAN PANEN</th>
-              <th className="border p-2">REALISASI LUAS PANEN (HA)</th>
-              <th className="border p-2">REALISASI PRODUKSI PANEN (KW)</th>
-              {/* Tanaman Hortikultura Tahunan */}
-              {/* KOMODITAS	JUMLAH TANAMAN (POHON)	PRAKIRAAN BULAN PANEN	PRAKIRAAN PRODUKSI PANEN (KW)	REALISASI PANEN (POHON)	REALISASI PRODUKSI PANEN (KW) */}
-              <th className="border p-2">KOMODITAS</th>
-              <th className="border p-2">LUAS TANAM (HA)</th>
-              <th className="border p-2">PRAKIRAAN BULAN PANEN</th>
-              <th className="border p-2">PRAKIRAAN PRODUKSI PANEN (KW)</th>
-              <th className="border p-2">REALISASI LUAS PANEN (HA)</th>
-              <th className="border p-2">REALISASI PRODUKSI PANEN (KW)</th>
-            </tr>
-          </thead>
-          <tbody>
-            {resp?.data.map((item) => {
-              const kategori = item.kategori.toLocaleLowerCase();
-              return (
-                <tr key={item.id}>
-                  <td className="border p-2">{item.fk_kelompokId}</td>
-                  {/* TODO: Kecamatan  */}
-                  <td className="border p-2">{item.kelompok?.kecamatan}</td>
-                  <td className="border p-2">{item.kelompok?.desa}</td>
-                  {/* TODO: Lahan baku itu apa? */}
-                  <td className="border p-2">{item.luasLahan}</td>
-                  <td className="border p-2">{item.kelompok?.gapoktan}</td>
-                  <td className="border p-2">{item.kelompok?.namaKelompok}</td>
-                  {/* Tanaman Pangan */}
-                  {kategori.includes('pangan') ? (
-                    <>
-                      <td className="border p-2 capitalize">{item.komoditas}</td>
-                      <td className="border p-2">{item.luasLahan}</td>
-                      <td className="border p-2">{item.periodeTanam}</td>
-                      <td className="border p-2">{item.prakiraanBulanPanen}</td>
-                      <td className="border p-2">{item.prakiraanLuasPanen}</td>
-                      <td className="border p-2">{item.prakiraanHasilPanen}</td>
-                      <td className="border p-2">{item.realisasiLuasPanen ?? '-'}</td>
-                      <td className="border p-2">{item.realisasiHasilPanen ?? '-'}</td>
-                    </>
-                  ) : (
-                    <BlankCell numberOfCol={8} />
-                  )}
-                  {/* Tanaman Perkebunan Semusim */}
-                  {kategori.includes('kebun') &&
-                  komoditasSemusim.includes(item.komoditas.replace('Buah ', '')) ? (
-                    <>
-                      <td className="border p-2 capitalize">{item.komoditas}</td>
-                      <td className="border p-2">{item.luasLahan}</td>
-                      <td className="border p-2">{item.periodeTanam}</td>
-                      <td className="border p-2">{item.prakiraanBulanPanen}</td>
-                      <td className="border p-2">{item.prakiraanLuasPanen}</td>
-                      <td className="border p-2">{item.prakiraanHasilPanen}</td>
-                      <td className="border p-2">{item.realisasiLuasPanen ?? '-'}</td>
-                      <td className="border p-2">{item.realisasiHasilPanen ?? '-'}</td>
-                    </>
-                  ) : (
-                    <BlankCell numberOfCol={8} />
-                  )}
-                  {/* Tanaman Perkebunan Tahunan */}
-                  {kategori.includes('kebun') &&
-                  komoditasTahunan.includes(item.komoditas.replace('Sayur ', '')) ? (
-                    <>
-                      <td className="border p-2 capitalize">{item.komoditas}</td>
-                      <td className="border p-2">{item.luasLahan}</td>
-                      {/* GAADA PERIODE TANAM? */}
-                      <td className="border p-2">{item.prakiraanBulanPanen}</td>
-                      <td className="border p-2">{item.prakiraanLuasPanen}</td>
-                      <td className="border p-2">{item.prakiraanHasilPanen}</td>
-                      <td className="border p-2">{item.realisasiLuasPanen ?? '-'}</td>
-                      {/* GAADA REALISASI HASIL PANEN? */}
-                    </>
-                  ) : (
-                    <BlankCell numberOfCol={6} />
-                  )}
-                  {/* Tanaman Hortikurtira Semusim */}
-                  {(kategori.includes('sayur') || kategori.includes('buah')) &&
-                  komoditasSemusim.includes(item.komoditas.replace('Buah ', '')) ? (
-                    <>
-                      <td className="border p-2 capitalize">{item.komoditas}</td>
-                      <td className="border p-2">{item.luasLahan}</td>
-                      <td className="border p-2">{item.periodeTanam}</td>
-                      <td className="border p-2">{item.prakiraanBulanPanen}</td>
-                      <td className="border p-2">{item.prakiraanLuasPanen}</td>
-                      <td className="border p-2">{item.prakiraanHasilPanen}</td>
-                      <td className="border p-2">{item.realisasiBulanPanen ?? '-'}</td>
-                      <td className="border p-2">{item.realisasiLuasPanen ?? '-'}</td>
-                      <td className="border p-2">{item.realisasiHasilPanen ?? '-'}</td>
-                    </>
-                  ) : (
-                    <BlankCell numberOfCol={9} />
-                  )}
-                  {/* Tanaman Hortikurtira Tahunan */}
-                  {(kategori.includes('sayur') || kategori.includes('buah')) &&
-                  komoditasTahunan.includes(item.komoditas.replace('Sayur ', '')) ? (
-                    <>
-                      <td className="border p-2 capitalize">{item.komoditas}</td>
-                      <td className="border p-2">{item.luasLahan}</td>
-                      <td className="border p-2">{item.prakiraanBulanPanen}</td>
-                      <td className="border p-2">{item.prakiraanHasilPanen}</td>
-                      <td className="border p-2">{item.realisasiLuasPanen ?? '-'}</td>
-                      <td className="border p-2">{item.realisasiHasilPanen ?? '-'}</td>
-                    </>
-                  ) : (
-                    <BlankCell numberOfCol={6} />
-                  )}
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+      <div>
+        <p className="text-white text-2xl font-bold flex justify-center mt-4">
+          {completeMessage ? completeMessage : 'Sedang mengunduh data, mohon tunggu sebentar...'}
+        </p>
       </div>
     </div>
-  );
-}
-
-function BlankCell({ numberOfCol }: { numberOfCol: number }) {
-  return (
-    <>
-      {Array.from({ length: numberOfCol }).map((_, index) => (
-        <td key={index} className="border p-2">
-          -
-        </td>
-      ))}
-    </>
   );
 }
